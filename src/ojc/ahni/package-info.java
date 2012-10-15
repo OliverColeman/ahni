4440c926afb60cc6b6ddeaedd2979ba24d72912b
Oliver J. Coleman <oliver.coleman@gmail.com>
2012-10-15 14:10:31 +1100

    Added JavaDoc for ojc.ahni package.

---
+++ src/ojc/ahni/package-info.java
@@ -0,0 +1,11 @@
+/**
+<p><strong>AHNI - Another HyperNEAT Implementation.</strong></p>
+
+<p>AHNI implements the <a href="http://eplex.cs.ucf.edu/hyperNEATpage/HyperNEAT.html">HyperNEAT</a> neuroevolution algorithm.</p>
+<p>The latest version is available at, and issues should be posted on, the 
+<a href="https://github.com/OliverColeman/ahni">GitHub repository</a>.</p>
+
+<p>See <a href="http://olivercoleman.github.com/ahni">here</a> for an overview and information on getting started and creating new experiments.</p>
+ */
+
+package ojc.ahni;
